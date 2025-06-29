3e9048f782de81988726b4054c9c864104122c6a
Adam Blumenfeld <adamb@csxlabs.org>
2025-06-29 11:54:43 -0700

    feat: complete CMake with module 3.1

---
+++ c-cpp/3.1/include/my_project/utils.h
@@ -0,0 +1,6 @@
+#ifndef MYPROJECT_UTILS_H
+#define MYPROJECT_UTILS_H
+
+void print_message();
+
+#endif
---
+++ c-cpp/3.1/src/main.cc
@@ -0,0 +1,7 @@
+#include "my_project/utils.h"
+
+int main() {
+  print_message();
+  return 0;
+}
+
